774bb091dc053eb8bfc501024420fd7f39179162
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-10-18 10:28:54 +0600

    added brand name and image

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
 
-// Brand-shop, mMFpnNJZRRcBacsj
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://Brand-shop:mMFpnNJZRRcBacsj@cluster0.7jyxnen.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7jyxnen.mongodb.net/?retryWrites=true&w=majority`;
 
+console.log(uri)
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -23,12 +23,41 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
+// brand name and img
+const brands =[
+  {
+    name:"iphone",
+    image:"https://i.ibb.co/dQQ4GSP/AV-IPHONE15-OFF.webp"
+  },
+  {
+    name:"Samsung",
+    image:"https://i.ibb.co/nc1Zfv6/Samsung-Logo-2.png"
+  },
+  {
+    name:"Realme",
+    image:"https://i.ibb.co/82P3YmS/Realme-logo.png"
+  },
+  {
+    name:"Xiomi",
+    image:"https://i.ibb.co/ZgK70R0/Xiaomi-logo-svg.png"
+  },
+  {
+    name:"Google Pixel",
+    image:"https://i.ibb.co/cNzM3s5/google-pixel-4-logo-is-seen-at-a-store-in-mountain-view-california-on-november-2-2019-google-pixel-4.jpg"
+  },
+  {
+    name:"Asus Rog",
+    image:"https://i.ibb.co/X22bHq4/asus-rog-phone-3-immagini-render-leak-smartphone-gaming-2.jpg"
+  },
+]
 async function run() {
   try {
 
     await client.connect();
+    const brandCollection = client.db("BuyPhoneDB").collection('brands')
 
+    const result = await brandCollection.insertMany(brands)
+    console.log(`Inserted ${result.insertedCount} documents`)
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
