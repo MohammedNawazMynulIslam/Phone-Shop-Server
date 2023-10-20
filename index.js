0ad513afbdcc0f1df5bddb8f9842b4053864a09a
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-10-20 19:42:17 +0600

    added delete method

--- index.js
+++ index.js
@@ -112,7 +112,19 @@ app.post('/addCart',async(req,res)=>{
   res.send(result)
 })
 
-
+// show carted product
+app.get('/addCart',async(req,res)=>{
+  const cursor = addToCartCollection.find()
+  const result = await cursor.toArray()
+  res.send(result)
+})
+// delete product
+app.delete('/addCart/:id',async(req,res)=>{
+  const id = req.params.id
+  const query = {_id:new ObjectId(id)}
+  const result = await addToCartCollection.deleteOne(query)
+  res.send(result)
+})
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
